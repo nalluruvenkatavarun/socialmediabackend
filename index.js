45a355a577d340b951dc5d2e9b6e90df40c04e93
varun <varun.nalluru@outlook.com>
2022-07-30 14:36:41 +0530

    commiting

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.get('/',(req,res) =>{
+  res.send("Hello to memories API");
+});
 const CONNECTION_URL = 'mongodb+srv://varun:varun@cluster0.6le0r.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
